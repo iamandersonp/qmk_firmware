80e5c6a5579e506fcd48fa30073a0d1713640b3a
Anderson Peñaloza <designeromega@gmail.com>
2024-01-16 09:05:59 +0100

    fix: :bug: fix oled display

---
+++ keyboards/crkbd/keymaps/iamandersonp/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight. Do not enable this with RGBLIGHT_ENABLE.
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+WPM_ENABLE = yes
